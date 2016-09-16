1c109a49e35da82bd6d01a4eff5640efd50f4e1d
gnardini <gonzalo.nardini.dev@gmail.com>
2016-09-15 23:39:03 -0300

    Adds real-time drawing using action cable

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,15 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_user_id
+    end
+
+    protected
+
+    def find_user_id
+      @user_id ||= 1 + rand(10000)
+    end
   end
 end
