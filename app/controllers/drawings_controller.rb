5b28d1275507487764b1f5c56e9c9687c0b42774
gnardini <gonzalo.nardini.dev@gmail.com>
2016-09-15 22:46:44 -0300

    Adds bulk insert to make query more efficient

--- app/controllers/drawings_controller.rb
+++ app/controllers/drawings_controller.rb
@@ -4,10 +4,25 @@ class DrawingsController < ApplicationController
   end
 
   def create
+    insert_with_raw_sql
+    # insert_normally
+  end
+
+  def insert_normally
     points = []
     params[:points].each do |key, value|
       points << Point.new(x: value[:x], y: value[:y])
     end
     Drawing.create(points: points)
+  end
+
+  def insert_with_raw_sql
+    drawing = Drawing.create
+    points = []
+    params[:points].each do |key, value|
+      points << "(#{value[:x]}, #{value[:y]}, #{drawing.id})"
+    end
+    sql = "INSERT INTO points (`x`, `y`, `drawing_id`) VALUES #{points.join(", ")}"
+    ActiveRecord::Base.connection.execute sql
   end
 end
